5625232c922f388d19c094732c667ae159ba6ace
DAVID BOGUI <franckbogui@gmail.com>
2021-04-29 16:33:25 +0000

    worker tontine periodicity okay

--- src/main/kotlin/com/b2i/tontine/infrastructure/db/repository/TontinePeriodicityRepository.kt
+++ src/main/kotlin/com/b2i/tontine/infrastructure/db/repository/TontinePeriodicityRepository.kt
@@ -1,5 +1,6 @@
 package com.b2i.tontine.infrastructure.db.repository
 
+import com.b2i.tontine.domain.tontine.entity.Tontine
 import com.b2i.tontine.domain.tontine_periodicity.entity.TontinePeriodicity
 import org.springframework.data.jpa.repository.JpaRepository
 
@@ -8,4 +9,8 @@ interface TontinePeriodicityRepository:JpaRepository<TontinePeriodicity,Long> {
     fun findAllByPeriodicityState(state:String): MutableList<TontinePeriodicity>
 
     fun countAllByBiddingState(state: String): Long
+
+    fun countAllByTontine(tontine:Tontine):Long
+
+    fun findAllByTontine(tontine: Tontine): MutableList<TontinePeriodicity>
 }
